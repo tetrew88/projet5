439aff16c71079e5370c2a887eab8c5ce93cf4a4
tetrew88 <45017955+tetrew88@users.noreply.github.com>
2019-09-05 17:29:13 +0200

    add product selection

--- main.py
+++ main.py
@@ -4,17 +4,15 @@ import function
 import mysql.connector
 
 from classes.category import *
+from classes.product import *
 
-
-host = "*****"
-user = "*****"
-password = "*****"
-database = "*****"
+host = "localhost"
+user = "donovan"
+password = "doni88650"
+database = "OpenFoodFact"
 
 
 menu_choice = 0
-
-print("Bienvenu dans eatBetter")
 
 #connection to the database
 connection = mysql.connector.connect(
@@ -42,7 +40,7 @@ if menu_choice == 1:
     if category_choice == False:
         print("Any data")
     else:
-        product_choice = Product.select_a_product(cursor)
+        product_choice = Product.select_a_product(cursor, category_choice)
         if product_choice == False:
             print("Any data")
         else:
